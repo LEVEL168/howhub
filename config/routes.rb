77cf7820dbc78b16f5e41ce84a4279415c421913
yuina-honda <ec2-user@ip-172-31-24-207.us-west-2.compute.internal>
2023-03-07 17:33:45 +0000

    トップページのHTML,CSS適用

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root to:"crafts#look"
     get "/new", to:"crafts#new"
     post "/create", to:"crafts#create"
-    get "/index", to:"crafts#index"
+    get "/top", to:"homes#top"
 end
