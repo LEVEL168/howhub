75d98379b3aeac073e152d6f96a7c229441d1a27
yuina-honda <ec2-user@ip-172-31-24-207.us-west-2.compute.internal>
2023-04-13 17:59:54 +0000

    ハンバーガー閉じるアクション以外実装

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,22 +1,47 @@
-window.onload = function () {
-　alert("javascript home");
-};
-
-'use strict';
-
-{
-    const open = document.getElementById('open');
-    const overlay = document.querySelector('.overlay');
-    const close = document.getElementById('close');
-    
-    open.addEventListener('click', () => {
-       overlay.classList.add('show'); 
-       open.classList.add('hide');
+/*global $*/
+$(function(){
+    $('#open').click(function(){
+       $('.overlay, nav').toggleClass('show'); 
+       if($('.overlay').hasClass('show')){
+           $('.fa-bars').hide();
+           $('.fa-times').show();
+       }else{
+           $('.fa-times').hide();
+           $('.fa-bars').show();
+       }
+       
+       $('.overlay').toggleClass('is-active');
     });
+});
+
+//   $('.nav-btn').on('click', function(){
+//     $('.overlay').toggleClass('is-active');
+//   });
+
+// $('#open').on('click', function() {
+//         $('.overlay, nav').toggleClass('show');
+//         // $('.fa-xmark').toggleClass('show');
+//     });
     
-    close.addEventListener('click', () => {
-       overlay.classList.remove('show'); 
-       open.classList.remove('hide');
-    });
+//     $('#close').on('click', function() {
+//         $('.sp-nav').toggleClass('hide');
+//     });
+
+// 'use strict';
+
+// {
+//     const open = document.getElementById('open');
+//     const overlay = document.querySelector('.overlay');
+//     const close = document.getElementById('close');
     
-}
+//     open.addEventListener('click', () => {
+//       overlay.classList.add('show'); 
+//       open.classList.add('hide');
+//     });
+    
+//     close.addEventListener('click', () => {
+//       overlay.classList.remove('show'); 
+//       open.classList.remove('hide');
+//     });
+    
+// }
