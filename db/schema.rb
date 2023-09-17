39bdb3bd678620fff8bdf1874d8e6afd9fd5576b
Yuina Honda <ec2-user@ip-172-31-24-207.us-west-2.compute.internal>
2023-09-17 11:51:39 +0000

    不要なマイグレーションファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2023_09_14_130013) do
     t.index ["user_id"], name: "index_thanks_on_user_id"
   end
 
+  create_table "user_crafts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "craft_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "mail"
