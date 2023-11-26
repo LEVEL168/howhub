b46e8b36ad04d16e873208a3396f5658cb1026f9
Yuina Honda <ec2-user@ip-172-31-24-207.us-west-2.compute.internal>
2023-11-26 14:20:55 +0000

    Ajax添削用

--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -2,14 +2,14 @@ class ThanksController < ApplicationController
     before_action :require_login
     
     def create
-       craft = Craft.find(params[:user_id])
-       thank = current_user.thanks.new(craft_id: craft.id)
+       @craft = Craft.find(params[:user_id])
+       thank = current_user.thanks.new(craft_id: @craft.id)
        thank.save
     end
     
     def destroy
-        craft = Craft.find(params[:user_id])
-        thank = current_user.thanks.find_by(craft_id: craft.id)
+        @craft = Craft.find(params[:user_id])
+        thank = current_user.thanks.find_by(craft_id: @craft.id)
         thank.destroy
     end
 end
