ab891d0bf1a82355a582a16d88f58412f9d93831
Yuina Honda <ec2-user@ip-172-31-24-207.us-west-2.compute.internal>
2023-12-01 17:41:19 +0000

    いいね機能添削用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_08_145041) do
+ActiveRecord::Schema.define(version: 2023_12_01_170314) do
 
   create_table "crafts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2023_10_08_145041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "thanks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "craft_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
