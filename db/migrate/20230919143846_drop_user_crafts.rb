5cd4488ee678c160beff3bb632afd4f3dc49b94f
Yuina Honda <ec2-user@ip-172-31-24-207.us-west-2.compute.internal>
2023-12-01 13:58:20 +0000

    マイグレーションファイル修正

--- db/migrate/20230919143846_drop_user_crafts.rb
+++
@@ -1,5 +0,0 @@
-class DropUserCrafts < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :user_crafts
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -21,16 +21,6 @@ ActiveRecord::Schema.define(version: 2023_10_08_145041) do
     t.integer "user_id"
   end
 
-  create_table "thanks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "craft_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["craft_id"], name: "index_thanks_on_craft_id"
-    t.index ["user_id", "craft_id"], name: "index_thanks_on_user_id_and_craft_id", unique: true
-    t.index ["user_id"], name: "index_thanks_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "mail"
