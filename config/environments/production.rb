e85bc1e9def4c0ca3f455f14dd416bcdcb5131b1
LEVEL168 <LEVEL168bit@gmail.com>
2024-04-26 17:54:08 +0000

    メーラーの本番環境修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "howhub_#{Rails.env}"
   config.action_mailer.perform_caching = false
   
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.asset_host = 'http://localhost:3000'
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
